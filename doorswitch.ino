bb6e71513c5f69b1bc6109a6f75cf272753bb831
John Amos <jlajr36@gmail.com>
2017-11-05 08:19:38 -0500

    Add files via upload

---
+++ doorswitch.ino
@@ -0,0 +1,13 @@
+int inPin = 2;
+int state = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(inPin, INPUT);
+}
+
+void loop() {
+  state = digitalRead(inPin);
+  Serial.println(state);
+  delay(100);
+}
